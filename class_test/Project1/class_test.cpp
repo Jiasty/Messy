86e3d27b2ab5d81057af9d3d7014599c53a16e3d
jiasty <2739142322@qq.com>
2024-09-24 23:27:12 +0800

    update

--- class_test/Project1/class_test.cpp
+++ class_test/Project1/class_test.cpp
@@ -1,25 +1,47 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <iostream>
 
-class Date
-{};
-
 class A
 {
 public:
-	void func()
-	{
-		return;
-	}
+	A(const A& a)
+	{}
+
+private:
+	int _id;
 };
 
 int main()
 {
 	std::cout << 1;
-	std::cout << sizeof(Date) << std::endl;
+	//只有拷贝构造时
+	//A a1(); //编译可通过
 
-	A* p = nullptr;
-	//p->func(); //正常运行，func()在公共区域，没有在类里面，编译时就已经确认地址。
-	//vs无法运行成功也许是MSVC编译器检测太严格。
+	//A a2; //无法构造
+
 	return 0;
 }
+
+
+
+//空类大小、空指针引用问题
+//class Date
+//{};
+//
+//class A
+//{
+//public:
+//	void func()
+//	{
+//		return;
+//	}
+//};
+//
+//int main()
+//{
+//	std::cout << "空类的大小为" << sizeof(Date) << std::endl;
+//
+//	A* p = nullptr;
+//	p->func(); //正常运行，func()在公共区域，没有在类里面，编译时就已经确认地址。
+//	return 0;
+//}
